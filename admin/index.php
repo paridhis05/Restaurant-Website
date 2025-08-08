020da36c75ac5f210a68c372e85aaacd818dc3ee
Paridhi Saini <paridhisaini19@gmail.com>
2025-08-08 19:53:35 +0530

    Add content

--- admin/index.php
+++ admin/index.php
@@ -4,8 +4,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
+    <link rel="stylesheet" href="../css/admin.css">
 </head>
 <body>
-    <h1>Admin Panel</h1>
+    <!-- <h1>Admin Panel</h1> -->
+
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Dashboard</h1>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br/>
+                Categories
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br/>
+                Categories
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br/>
+                Categories
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br/>
+                Categories
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2020 All rights reserved, Some Restaurant Developed By - Paridhi Saini</p>
+        </div>
+    </div>
 </body>
 </html>
