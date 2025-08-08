5f7be87ebed775e249d0c5d765cb64f8daca143b
Paridhi Saini <paridhisaini19@gmail.com>
2025-08-08 19:58:11 +0530

    Add Manage Admin Page

---
+++ admin/manage-admin.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manage-Admin</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!-- <h1>Admin Panel</h1> -->
+
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Admin</h1>
+
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2020 All rights reserved, Some Restaurant Developed By - Paridhi Saini</p>
+        </div>
+    </div>
+</body>
+</html>
