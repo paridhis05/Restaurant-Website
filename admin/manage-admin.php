b7d1856476498a8470d88a1f7622b5b59d71cf16
Paridhi Saini <paridhisaini19@gmail.com>
2025-08-09 17:28:22 +0530

    Update menu and footer

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -1,26 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manage-Admin</title>
-    <link rel="stylesheet" href="../css/admin.css">
-</head>
-<body>
-    <!-- <h1>Admin Panel</h1> -->
-
-    <div class="menu text-center">
-        <div class="wrapper">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Admin</a></li>
-                <li><a href="#">Category</a></li>
-                <li><a href="#">Food</a></li>
-                <li><a href="#">Order</a></li>
-            </ul>
-        </div>
-
-    </div>
+<?php
+    include('partials/menu.php');
+?>
 
     <div class="main-content">
         <div class="wrapper">
@@ -29,10 +9,6 @@
         </div>
     </div>
 
-    <div class="footer">
-        <div class="wrapper">
-            <p class="text-center">2020 All rights reserved, Some Restaurant Developed By - Paridhi Saini</p>
-        </div>
-    </div>
-</body>
-</html>
+<?php
+    include('partials/footer.php');
+?>
