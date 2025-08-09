3ccd34c31507770ad430da072037584fe3ecb835
Paridhi Saini <paridhisaini19@gmail.com>
2025-08-09 17:26:26 +0530

    Set menu andFooter seperate

---
+++ admin/partials/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2020 All rights reserved, Food House. Developed By - Paridhi Saini</p>
+        </div>
+    </div>
+</body>
+</html>
