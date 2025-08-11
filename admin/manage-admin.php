ddcf64bd445af274e7a743f3cd76951e3d96b4ce
Paridhi Saini <paridhisaini19@gmail.com>
2025-08-11 22:17:07 +0530

    Add Table

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -5,6 +5,51 @@
     <div class="main-content">
         <div class="wrapper">
             <h1>Manage Admin</h1>
+            <br/><br/>
+
+            <!-- button to add admin -->
+             <a href="#" class="btn-primary">Add admin</a>
+             <br/><br/><br/>
+             
+            <table class="tbl-full">
+                <tr>
+                    <th>S.no.</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Action</th>
+                </tr>
+
+                <tr>
+                    <td>1.</td>
+                    <td>Paridhi S.</td>
+                    <td>paridhis</td>
+                    <td>
+                        Update Admin
+                        Delete Admin
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>2.</td>
+                    <td>Paridhi S.</td>
+                    <td>paridhis</td>
+                    <td>
+                        Update Admin
+                        Delete Admin
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>3.</td>
+                    <td>Paridhi S.</td>
+                    <td>paridhis</td>
+                    <td>
+                        Update Admin
+                        Delete Admin
+                    </td>
+                </tr>
+
+            </table>
 
         </div>
     </div>
